306e3161a9ca82bacc9d4907a369f92fbc1c19a1
Hun191189 <tranmanhhung191189@gmai.com>
2023-10-06 08:01:43 +0700

    create new repo

--- src/main/java/com/example/md3_case_demo1/model/Posts.java
+++
@@ -1,7 +0,0 @@
-package com.example.md3_case_demo1.model;
-
-public class Posts {
-    private int idPosts;
-    private String titlePosts;
-    private String content;
-}
--- src/main/java/com/example/md3_case_demo1/model/User.java
+++
@@ -1,9 +0,0 @@
-package com.example.md3_case_demo1.model;
-
-import java.io.PrintWriter;
-
-public class User {
-    private int id;
-    private String name;
-    private int age;
-}
